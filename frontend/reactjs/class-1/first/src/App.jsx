7653a5cd118a4ad1fcbc9374d3dd67ba4776b5e8
bharat <bharatchoutharyb@gmail.com>
2026-01-09 20:13:19 +0530

    done

--- frontend/reactjs/class-1/first/src/App.jsx
+++ frontend/reactjs/class-1/first/src/App.jsx
@@ -1,12 +1,119 @@
-import React from 'react'
 import Card from './components/Card'
 
 const App = () => {
+
+  const jobOpenings = [
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/0/05/Meta_Platforms_Inc._logo.svg",
+    companyName: "Meta",
+    datePosted: "5 days ago",
+    post: "Frontend Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$65/hour",
+    location: "Menlo Park, USA"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+    companyName: "Amazon",
+    datePosted: "2 weeks ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$70/hour",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+    companyName: "Apple",
+    datePosted: "3 weeks ago",
+    post: "iOS Developer",
+    tag1: "Part Time",
+    tag2: "Senior Level",
+    pay: "$90/hour",
+    location: "Cupertino, USA"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+    companyName: "Netflix",
+    datePosted: "10 days ago",
+    post: "Machine Learning Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$110/hour",
+    location: "Los Gatos, USA"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+    companyName: "Google",
+    datePosted: "1 week ago",
+    post: "Cloud Solutions Architect",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$85/hour",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    companyName: "Microsoft",
+    datePosted: "4 weeks ago",
+    post: "Data Scientist",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$75/hour",
+    location: "Redmond, USA"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg",
+    companyName: "IBM",
+    datePosted: "2 days ago",
+    post: "AI Research Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$95/hour",
+    location: "New York, USA"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg",
+    companyName: "Tesla",
+    datePosted: "6 days ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$80/hour",
+    location: "Austin, USA"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/2/21/Nvidia_logo.svg",
+    companyName: "NVIDIA",
+    datePosted: "3 weeks ago",
+    post: "GPU Programmer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$120/hour",
+    location: "Santa Clara, USA"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg",
+    companyName: "Oracle",
+    datePosted: "8 days ago",
+    post: "Database Administrator",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$70/hour",
+    location: "Mumbai, India"
+  }
+];
+
+
+
   return (
     <div className='parent'>
-      <Card user='Aman Singh' age={18} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
-      <Card user='Sarthak' age={28} img='https://images.unsplash.com/photo-1752801516481-cfb8c47ee9a3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
-      <Card user='Aditya' age={24} img='https://images.unsplash.com/photo-1751554933476-d029737d58b2?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      {jobOpenings.map(function (elem, idx) {
+        return <div key={idx}>
+          <Card company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} brandLogo={elem.brandLogo} pay={elem.pay} tag2={elem.tag2} />
+        </div>
+      })}
     </div>
   )
 }
